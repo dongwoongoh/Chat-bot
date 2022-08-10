c95c1a9787e20536d9d191f8e135e17fc02d2b0f
maaaaaaaaad <integral5137@gmail.com>
2022-08-10 22:13:55 +0900

    on init prisma and befor exit cycle

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -10,12 +10,13 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    console.log('on prisma init');
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
-      console.log('before exit!');
+      console.log('before prisma exit!');
       await app.close();
     });
   }
