c4d2aba482150915bdddeb81d90516b4012ad342
maaaaaaaaad <integral5137@gmail.com>
2022-08-14 11:25:11 +0900

    stash dtos

--- src/users/dtos/users.sign.up.dto.ts
+++ src/users/dtos/users.sign.up.dto.ts
@@ -1,12 +1,17 @@
-import { BaseOutputDto } from "../../common/dtos/base.output.dto";
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { BaseOutputDto } from '../../common/dtos/base.output.dto';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UsersSignUpInputDto {
   @IsEmail()
   @IsNotEmpty()
-  email: string
+  email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 
+  @IsBoolean()
+  alram: boolean;
 }
 
-export class UsersSignUpOutputDto extends BaseOutputDto<any>{}
+export class UsersSignUpOutputDto extends BaseOutputDto<any> {}
