ce6d490702abc19513ec506ab6a5ea7479f651ee
maaaaaaaaad <integral5137@gmail.com>
2024-07-13 23:50:54 +0900

    e

--- main.tf
+++ main.tf
@@ -9,18 +9,57 @@ terraform {
 
 provider "docker" {}
 
+resource "docker_network" "src" {
+  name = "mad"
+}
+
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
+  depends_on   = [docker_network.src]
+}
+
+resource "docker_image" "psql" {
+  name         = "postgres:14.12"
+  keep_locally = false
+  depends_on   = [docker_image.nginx]
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
-
+  name  = "web-server"
   ports {
     internal = 80
-    external = 8000
+    external = 80
     protocol = "tcp"
   }
+  networks_advanced {
+    name = docker_network.src.name
+  }
+  cpu_shares = 512
+  memory     = 512
+  restart    = "always"
+  depends_on = [docker_image.nginx]
+}
+
+resource "docker_container" "db" {
+  image = docker_image.psql.image_id
+  name  = "database"
+  ports {
+    internal = 5432
+    external = 5432
+    protocol = "tcp"
+  }
+  networks_advanced {
+    name = docker_network.src.name
+  }
+  cpu_shares = 1024
+  memory     = 1024
+  env        = [
+    "POSTGRES_DB=${var.db_name}",
+    "POSTGRES_USER=${var.db_user}",
+    "POSTGRES_PASSWORD=${var.db_password}"
+  ]
+  restart    = "always"
+  depends_on = [docker_image.psql]
 }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "db_name" {
+  type    = string
+  default = "terraform-psql"
+}
+
+variable "db_user" {
+  type    = string
+  default = "postgres"
+}
+
+variable "db_password" {
+  type    = string
+  default = "qwe123123!"
+}
